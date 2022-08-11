8a9691189aaefa722e32df5c6133d6e997496eca
Paulo cesar <paulocesar.n.dosantos@gmail.com>
2022-08-11 13:35:50 -0300

    #Alterado email e senha do adm#

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -11,7 +11,7 @@
 
 	//usuarios do sistema
 	$usuarios_app = array(
-		array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 1, 'email' => 'adm@teste.com', 'senha' => '123', 'perfil_id' => 1),
 		array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
 		array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
 		array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
